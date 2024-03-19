ecffd33d20dcd6550d5b06d0541a61a8318b290e
murasuke <42880820+murasuke@users.noreply.github.com>
2024-03-19 12:41:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { comlink } from 'vite-plugin-comlink';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? 'react_vite_comlink' : './',
   plugins: [comlink(), react()],
   worker: {
     plugins: [comlink()],
